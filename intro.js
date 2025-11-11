b8d7e35673436382dfa6db680e0c3db989a1b398
zoheb17 <sayyedzohebuddin61859@gmail.com>
2025-11-11 12:56:55 +0000

    theory of nodejs

---
+++ intro.js
@@ -0,0 +1,83 @@
+// the beginning - 2009 rayan dahl's 
+// in 2009 a software engineer named rayan dahl from the us created node.js
+
+
+// at that time 
+
+// javaScript was only used  in browsefer
+// - servers were built using php,python or java 
+//  each request on the server would create a new thread  consuming memeor and slowing thins down under heavy load 
+
+// rayan found this inefficient especially after seeing how web browser handle  asynchronous even (non blocking operation
+
+// he wanted the same non -blocking behavior on the server )
+// that how Node,js was born to bring javscript to the backend but in a faster and more scable way  
+
+
+
+// the core idea - event driven & non - blocking i/o
+
+// rayan built Node.js using 
+
+// google v8 engine (the same engine that powers chrome )
+// libuv (a c libaray for asynchrouns i/)
+
+// this combination allowed Node.js to 
+// handle many request at once using a single thread 
+// avoid blocking while waitng for task like file reading or database queries 
+
+// in short 
+
+// early days (2009 - 2011) 
+// 2009 node js was    realsed at jsconf eu conference 
+// 2010 npm (note package manger ) was introuced - the game changer
+
+// developers could easliy install and share reusable  package 
+
+// 2001 companies like linkind and uber started using Node.js for their backends due to its speed and scalanity 
+
+// example 
+//  linkeind moved from ruby rail to node js and saw their server handle 10x more traffic wich fewer machines 
+
+
+// rise in popularity (2012- 2015)
+// node rapidly become go =to choice for building 
+// . api 
+// .real - time application (chat live updates)
+// .command line tools
+// developer utiles 
+
+// Big companies joined 
+
+// .Netflix(for streaming)
+// .paypal (for faster apis)
+// . eBay walmart amd nasa adopted node.js in production 
+
+
+// however.around 2014 there was community tension- deveopment was slow and contributors were fusrated 
+
+
+
+// ------------------------------------
+// Today echosystem 
+// . over 2 millon + package on npm 
+// . used by 90% + morden web developer 
+// .foundation for frameworks like express .js fastify and adonisJs  
+
+// its no longer "just javaScript on the server - 
+// its a backbone" of thr modern web connecting api microservies and tools 
+
+
+
+// summary timeline 
+/*
+year                      event 
+
+2009                     rayan dahl creats node.js  
+2010                     npm (node package manager ) is launched 
+2011                     linkedin & uber mnain 
+2014                     Io,js for due to governance issue 
+2015                     io .js  merges back - node  foundation formed 
+2017                     async/await es module and major improvemnts 
+2020                     node.js become a global standard for web servers 
+*/
